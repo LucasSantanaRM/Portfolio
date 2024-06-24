214931de9f7c4400e949bfa9b180285deadfe5ba
LucasSantanaRM <83544692+LucasSantanaRM@users.noreply.github.com>
2024-06-24 16:16:51 -0400

    teste

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -50,17 +50,7 @@ function Projects() {
               description="Implementação do software de monitoramento de rede zabbix  em 3 camadas + proxy para melhor desempenho do serviço"
                             
             />
-          </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
           </Col>
 
           <Col md={4} className="project-card">
